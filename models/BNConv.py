bfb4d04ae8ae01bfadff32fa55842e83785e1caf
Arunava <learningdroidarunava@gmail.com>
2019-03-11 11:17:14 +0530

    Added discriminator and updated BNConv

--- models/BNConv.py
+++ models/BNConv.py
@@ -4,10 +4,20 @@ import torch.nn as nn
 class BNConv(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride,
-                 padding, bias=False, eps=1e-5, momentum=0.1, conv_first=True, relu=True):
+                 padding, bias=False, eps=1e-5, momentum=0.1, conv_first=True, relu='relu', nslope=0.2, norm='batch'):
 
         super(BNConv, self).__init__()
         
+        relu = relu.lower()
+        norm = norm.lower()
+        
+        if norm == 'batch':
+            norm = nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum)
+        elif norm == 'instance':
+            norm = nn.InstanceNorm2d(out_channels, eps=eps, momentum=momentum)
+        else:
+            raise ('Norm value not understood')
+
         if conv_first:
             self.main = nn.ModuleList([
                             nn.Conv2d(in_channels=in_channels,
@@ -16,16 +26,17 @@ class BNConv(nn.Module):
                                       stride=stride,
                                       padding=padding,
                                       bias=bias),
-                            
-                            nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum)
+                            norm
                     ])
             
-            if relu:
+            if relu == 'relu':
                 self.main.append(nn.ReLU(inplace=True))
+            elif relu == 'leaky':
+                self.main.append(nn.LeakyReLU(negative_slope=nslope, inplace=True)
         else:
 
             self.main = nn.ModuleList(
-                            nn.BatchNorm2d(in_channels, eps=eps, momentum=momentum),
+                            norm,
 
                             nn.Conv2d(in_channels=in_channels,
                                       out_channels=out_channels,
@@ -35,8 +46,10 @@ class BNConv(nn.Module):
                                       bias=bias)
                     )
 
-            if relu:
+            if relu == 'relu':
                 self.main.insert(0, nn.ReLU(inplace=True))
+            elif relu == 'leaky':
+                self.main.insert(0, nn.LeakyReLU(negative_slope=nslope, inplace=True)
 
 
     def forward(self, x):
---
+++ models/Discriminator.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+from BNConv import BNConv
+
+class Discriminator(nn.Module):
+    '''
+    '''
+
+    def __init__(self, iconv=64, layers=4):
+        
+        super(Discriminator, self).__init__()
+        
+        self.net = nn.ModuleList([nn.Conv2d(in_channels=3,
+                                              out_channels=iconv,
+                                              kernel_size=4,
+                                              stride=2,
+                                              padding=1,
+                                              bias=False)
+                                ])
+
+        layers -= 1
+
+        for _ in range(layers):
+            self.net.append(BNConv(in_channels=iconv,
+                                  out_channels=iconv*2,
+                                  kernel_size=4,
+                                  stride=2,
+                                  padding=1,
+                                  relu='leaky',
+                                  nslope=0.2,
+                                  norm='instance')
+                            )
+
+            iconv *= 2
+
+        self.net.append(nn.Conv2d(in_channels=iconv,
+                                  out_channels=1,
+                                  kernel_size=2,
+                                  stride=1,
+                                  padding=0,
+                                  bias=False)
+
+
+    def forward(self, x):
+        
+        out = self.net(x)
+        return out
