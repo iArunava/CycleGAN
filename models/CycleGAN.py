a09b007fb22b8843e7b75faac906db66daf6717d
Arunava <learningdroidarunava@gmail.com>
2019-03-11 11:33:18 +0530

    Updated the CycleGAN model

--- models/CycleGAN.py
+++ models/CycleGAN.py
@@ -6,7 +6,7 @@ from ResidualBlock2L import ResidualBlock2L
 
 class CycleGAN(nn.Module):
 
-    def __init__(self):
+    def __init__(self, ic_conv=64):
         '''
         This class defines the network used for CycleGAN.
         This network is solely taken from the Supplementary material provided
@@ -16,76 +16,76 @@ class CycleGAN(nn.Module):
         
         self.net = nn.Sequential(
                         BNConv(in_channels=3,
-                                out_channels=32,
+                                out_channels=ic_conv,
                                 kernel_size=9,
                                 padding=4,
                                 stride=1,
                                 conv_first=True,
                                 relu=True),
 
-                        BNConv(in_channels=32,
-                                out_channels=64,
+                        BNConv(in_channels=ic_conv,
+                                out_channels=ic_conv*2,
                                 kernel_size=3,
                                 padding=1,
                                 stride=2,
                                 conv_first=True,
                                 relu=True),
 
-                        BNConv(in_channels=64,
-                                out_channels=128,
+                        BNConv(in_channels=ic_conv*2,
+                                out_channels=ic_conv*4,
                                 kernel_size=3,
                                 padding=1,
                                 stride=2,
                                 conv_first=True,
                                 relu=True),
 
-                        ResidualBlock2L(ic_conv=64,
-                                        oc_conv=128,
+                        ResidualBlock2L(ic_conv=ic_conv*4,
+                                        oc_conv=ic_conv*4,
                                         downsample=False,
                                         conv_first=True,
                                         relu_after_add=False),
 
-                        ResidualBlock2L(ic_conv=128,
-                                        oc_conv=128,
+                        ResidualBlock2L(ic_conv=ic_conv*4,
+                                        oc_conv=ic_conv*4,
                                         downsample=False,
                                         conv_first=True,
                                         relu_after_add=False),
 
-                        ResidualBlock2L(ic_conv=128,
-                                        oc_conv=128,
+                        ResidualBlock2L(ic_conv=ic_conv*4,
+                                        oc_conv=ic_conv*4,
                                         downsample=False,
                                         conv_first=True,
                                         relu_after_add=False),
 
-                        ResidualBlock2L(ic_conv=128,
-                                        oc_conv=128,
+                        ResidualBlock2L(ic_conv=ic_conv*4,
+                                        oc_conv=ic_conv*4,
                                         downsample=False,
                                         conv_first=True,
                                         relu_after_add=False),
 
-                        ResidualBlock2L(ic_conv=128,
-                                        oc_conv=128,
+                        ResidualBlock2L(ic_conv=ic_conv*4,
+                                        oc_conv=ic_conv*4,
                                         downsample=False,
                                         conv_first=True,
                                         relu_after_add=False),
 
-                        BNConvt(in_channels=128,
-                                out_channels=64,
+                        BNConvt(in_channels=ic_conv*4,
+                                out_channels=ic_conv*2,
                                 kernel_size=3,
                                 padding=1,
                                 stride=2,
                                 conv_first=True,
                                 relu=True),
 
-                        BNConvt(in_channels=64,
-                                out_channels=32,
+                        BNConvt(in_channels=ic_conv*2,
+                                out_channels=ic_conv,
                                 kernel_size=3,
                                 padding=1,
                                 stride=2,
                                 conv_first=True,
                                 relu=True),
 
-                        BNConvt(in_channels=32,
+                        BNConvt(in_channels=ic_conv,
                                 out_channels=3,
                                 kernel_size=9,
                                 padding=4,
